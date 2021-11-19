b0950961fc12000f46c046767a5ee424e3827ca7
Park-yu-su <pys01022342867@gmail.com>
2021-11-19 11:21:14 +0900

    실수로 여기다가 조금 코딩함

---
+++ app/src/main/java/com/example/englishvocabulary/ListWord.java
@@ -0,0 +1,14 @@
+package com.example.englishvocabulary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListWord extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_word);
+    }
+}
---
+++ app/src/main/java/com/example/englishvocabulary/ListWord_show.java
@@ -0,0 +1,14 @@
+package com.example.englishvocabulary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListWord_show extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_word_show);
+    }
+}
