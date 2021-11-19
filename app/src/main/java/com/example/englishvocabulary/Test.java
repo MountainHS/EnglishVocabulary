822e4cef14a35dabe003b3637f4196d93f65febb
Park-yu-su <pys01022342867@gmail.com>
2021-11-20 00:00:05 +0900

    잔 버그 수정 및 test 관련 xml 작성 완료

--- app/src/main/java/com/example/englishvocabulary/Test.java
+++ app/src/main/java/com/example/englishvocabulary/Test.java
@@ -19,7 +19,9 @@ public class Test extends AppCompatActivity {
         intent = getIntent();
         int many = intent.getIntExtra("ManyTestWord", 0);
 
-
-
+    /*
+    일단 ArrayList에 저장한 후, 현재 문제 변수 i, 정답 변수, 오답 변수, 전체 문제 변수=intent, 로 정한 다음
+    한 문제 풀고 나면, settext초기화 하고, 변수 늘리고 등등등 이용해서 종료하기 버튼을 누르거나 i==intent면 종료 화면으로 이동
+*/
     }
 }
